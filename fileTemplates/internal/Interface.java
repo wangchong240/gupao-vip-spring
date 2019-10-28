04022a7fa1ab90269cd238967e293409ce308a76
wangchong240 <1032788001@qq.com>
2019-10-28 22:37:23 +0800

    IU-2017.2.2 <dell@DESKTOP-TKOKVQO Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, Enum.java, Interface.java, other.xml, Default.xml, find.xml, ui.lnf.xml, lombok.xml, filetypes.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, mavenVersion.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+*@author Wang Chong at ${YEAR}-${MONTH}-${DAY} ${TIME}
+*@since V1.0.0
+*/
+public class ${NAME} {
+}
